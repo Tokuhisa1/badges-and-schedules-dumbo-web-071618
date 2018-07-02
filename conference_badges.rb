da77c4f555fe1b4a5ee3239f9cf6bd22aaa608b9
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-02 18:21:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
   messages = []
   array.each {|x| messages.push(badge_maker(x))}
+  messages
 end
