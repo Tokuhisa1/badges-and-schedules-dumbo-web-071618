6a964be882519d6755aef53927c3ee276e11282f
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-02 18:25:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,13 @@ def batch_badge_creator(array)
   array.each {|x| messages.push(badge_maker(x))}
   messages
 end
+
+def room_assigner(name, number)
+  "Hello, #{name}! You'll be assigned to room #{number}!"
+end
+
+def assign_rooms(array)
+  rooms = []
+  array.each {|y| rooms.push(room_assigner(y))}
+  rooms
+end
